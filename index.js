941dbbe68633485dd549bd5e5f2f135d4bd6eed2
Rodrigo Cardoso <rodrigo.cardoso1007@gmail.com>
2024-09-12 23:14:30 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ function gerarFaturaStr(fatura, pecas) {
     return total
   }
 
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }
+
   let totalFatura = 0;
   let creditos = 0;
   let faturaStr = `Fatura ${fatura.cliente}\n`;
@@ -35,16 +39,15 @@ function gerarFaturaStr(fatura, pecas) {
     }).format;
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    let total = calcularTotalApresentacao(apre, peca);
+    let total = calcularTotalApresentacao(apre, getPeca(apre));
 
     // créditos para próximas contratações
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia")
+    if (getPeca(apre).tipo === "comedia")
       creditos += Math.floor(apre.audiencia / 5);
 
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
   faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
